591f762ff18ed0da52d7dd09215b741ddcf2a5c0
Pillar.Liang <ymblender@gmail.com>
2023-08-24 23:12:15 -0700

    fix: change CI branch (#2)

--- app/public/sdk.js
+++ app/public/sdk.js
@@ -1,6 +1,6 @@
 const script = document.createElement("script");
 window.testflowyURL = "https://testflowy.com";
-script.src = "https://testflowy.com/sdk/index_fa2f8228e.js";
+script.src = "https://testflowy.com/sdk/index_d7de134f5.js";
 script.onload = function () {
   window.testflowySDK();
 };
--- app/public/sdk_dev.js
+++ app/public/sdk_dev.js
@@ -1,6 +1,6 @@
 const script = document.createElement("script");
 // window.testflowyURL = "https://testflowy.com";
-script.src = "/sdk/index_fa2f8228e.js";
+script.src = "/sdk/index_d7de134f5.js";
 script.onload = function () {
   window.testflowySDK({
     example: true,
